c4d4da556ee005aea4b21ae6b3f5a8cbd95f5546
TruptiG <t.gore@mail.cryst.bbk.ac.uk>
2023-08-21 17:29:57 +0100

    updated beautifuljekyll.js for navbar setting

--- assets/js/beautifuljekyll.js
+++ assets/js/beautifuljekyll.js
@@ -9,21 +9,21 @@ let BeautifulJekyllJS = {
     setTimeout(BeautifulJekyllJS.initNavbar, 10);
 
     // Shorten the navbar after scrolling a little bit down
-    $(window).scroll(function() {
-        if ($(".navbar").offset().top > 50) {
-            $(".navbar").addClass("top-nav-short");
-        } else {
-            $(".navbar").removeClass("top-nav-short");
-        }
-    });
+   // $(window).scroll(function() {
+      //  if ($(".navbar").offset().top > 50) {
+      //      $(".navbar").addClass("top-nav-short");
+      //  } else {
+      //      $(".navbar").removeClass("top-nav-short");
+      //  }
+   // });
 
     // On mobile, hide the avatar when expanding the navbar menu
-    $('#main-navbar').on('show.bs.collapse', function () {
-      $(".navbar").addClass("top-nav-expanded");
-    });
-    $('#main-navbar').on('hidden.bs.collapse', function () {
-      $(".navbar").removeClass("top-nav-expanded");
-    });
+   // $('#main-navbar').on('show.bs.collapse', function () {
+   //   $(".navbar").addClass("top-nav-expanded");
+   // });
+   // $('#main-navbar').on('hidden.bs.collapse', function () {
+   //   $(".navbar").removeClass("top-nav-expanded");
+   // });
 
     // show the big header image
     BeautifulJekyllJS.initImgs();
@@ -31,20 +31,20 @@ let BeautifulJekyllJS = {
     BeautifulJekyllJS.initSearch();
   },
 
-  initNavbar : function() {
+  //initNavbar : function() {
     // Set the navbar-dark/light class based on its background color
-    const rgb = $('.navbar').css("background-color").replace(/[^\d,]/g,'').split(",");
-    const brightness = Math.round(( // http://www.w3.org/TR/AERT#color-contrast
-      parseInt(rgb[0]) * 299 +
-      parseInt(rgb[1]) * 587 +
-      parseInt(rgb[2]) * 114
-    ) / 1000);
-    if (brightness <= 125) {
-      $(".navbar").removeClass("navbar-light").addClass("navbar-dark");
-    } else {
-      $(".navbar").removeClass("navbar-dark").addClass("navbar-light");
-    }
-  },
+  //  const rgb = $('.navbar').css("background-color").replace(/[^\d,]/g,'').split(",");
+   // const brightness = Math.round(( // http://www.w3.org/TR/AERT#color-contrast
+   //   parseInt(rgb[0]) * 299 +
+   //   parseInt(rgb[1]) * 587 +
+   //  parseInt(rgb[2]) * 114
+   // ) / 1000);
+   // if (brightness <= 125) {
+   //   $(".navbar").removeClass("navbar-light").addClass("navbar-dark");
+   // } else {
+   //   $(".navbar").removeClass("navbar-dark").addClass("navbar-light");
+   // }
+  //},
 
   initImgs : function() {
     // If the page was large images to randomly select from, choose an image
